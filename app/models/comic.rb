bb39879b0e01c8ad2610acaed53e12c30a8b25f4
João Proença <j-p-proenca@telecom.pt>
2022-07-24 22:03:36 +0100

    Added validation when theres no results

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -2,13 +2,17 @@ class Comic < ApplicationRecord
   require 'digest/md5'
   require 'rest-client'
 
+  BASE_URL = "https://gateway.marvel.com:443/v1/public/"
+
   def self.get_comics offset
     parsed_data = JSON.parse(call_api_fetch_comics(offset))
     parsed_data['data']['results'].map { |results| {"id" => results["id"], "title" => results["title"], "image" => image(results)} }
   end
   
   def self.get_comics_by_character(name, offset)
-    character_id = JSON.parse(call_api_fetch_character(name))['data']['results'][0]['id']
+    character = JSON.parse(call_api_fetch_character(name))
+    return {} if character['data']['count'] <= 0
+    character_id = character['data']['results'][0]['id']
     parsed_data = JSON.parse(call_api_fetch_comics_by_character(offset, character_id))
     parsed_data['data']['results'].map { |results| {"id" => results["id"], "title" => results["title"], "image" => image(results)} }
   end
@@ -16,15 +20,15 @@ class Comic < ApplicationRecord
   private
 
   def self.call_api_fetch_comics offset
-    RestClient.get("#{Rails.application.secrets.url}comics?apikey=#{Rails.application.credentials[:MARVEL_API][:PUBLIC_KEY]}&hash=#{Digest::MD5.hexdigest(Rails.application.credentials[:MARVEL_API][:HASH])}&ts=1234&orderBy=-onsaleDate&offset=#{offset}")
+    RestClient.get("#{BASE_URL}comics?apikey=#{Rails.application.credentials[:MARVEL_API][:PUBLIC_KEY]}&hash=#{Digest::MD5.hexdigest(Rails.application.credentials[:MARVEL_API][:HASH])}&ts=1234&orderBy=-onsaleDate&offset=#{offset}")
   end
 
   def self.call_api_fetch_character name
-    RestClient.get("#{Rails.application.secrets.url}characters?apikey=#{Rails.application.credentials[:MARVEL_API][:PUBLIC_KEY]}&hash=#{Digest::MD5.hexdigest(Rails.application.credentials[:MARVEL_API][:HASH])}&ts=1234&name=#{name}")
+    RestClient.get("#{BASE_URL}characters?apikey=#{Rails.application.credentials[:MARVEL_API][:PUBLIC_KEY]}&hash=#{Digest::MD5.hexdigest(Rails.application.credentials[:MARVEL_API][:HASH])}&ts=1234&name=#{name}")
   end
 
   def self.call_api_fetch_comics_by_character(offset, id_character)
-    RestClient.get("#{Rails.application.secrets.url}comics?apikey=#{Rails.application.credentials[:MARVEL_API][:PUBLIC_KEY]}&hash=#{Digest::MD5.hexdigest(Rails.application.credentials[:MARVEL_API][:HASH])}&ts=1234&orderBy=-onsaleDate&offset=#{offset}&characters=#{id_character}")
+    RestClient.get("#{BASE_URL}comics?apikey=#{Rails.application.credentials[:MARVEL_API][:PUBLIC_KEY]}&hash=#{Digest::MD5.hexdigest(Rails.application.credentials[:MARVEL_API][:HASH])}&ts=1234&orderBy=-onsaleDate&offset=#{offset}&characters=#{id_character}")
   end
 
 
