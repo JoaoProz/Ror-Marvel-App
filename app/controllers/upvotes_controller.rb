fd5a13abf7f779835c35934b94ce02f451517ef6
João Proença <j-p-proenca@telecom.pt>
2022-07-24 18:51:00 +0100

    Changes to show filtered page after upvote

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -3,11 +3,13 @@ class UpvotesController < ApplicationController
     def add_vote
         session[:comics_upvoted] = [] if session[:comics_upvoted].nil?
         session[:comics_upvoted] << params[:id]
+        return redirect_to '/comics/index?search=' + params[:search_term] if params[:search_term].present?
         redirect_to '/comics/index'
     end
 
     def remove_vote
         session[:comics_upvoted].delete(params[:id])
+        return redirect_to '/comics/index?search=' + params[:search_term] if params[:search_term].present?
         redirect_to '/comics/index'
 
     end
