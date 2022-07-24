0310cba33d9c1ddd7b0f61506e3816b80a9706da
João Proença <j-p-proenca@telecom.pt>
2022-07-24 22:54:14 +0100

    added route from main page to comics page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  get "", to: redirect('/comics/index')
   get 'comics/index'
-  get 'comics/by_character'
   post "add_vote", to: 'upvotes#add_vote'
   delete "remove_vote", to: 'upvotes#remove_vote'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
