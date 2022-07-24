5f3d3fa0b098fbf0729cc6395d8c7364537726f3
João Proença <j-p-proenca@telecom.pt>
2022-07-24 17:31:21 +0100

    Changes to get results per page

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,6 +1,8 @@
 class ComicsController < ApplicationController
   def index
-    @comics = Comic.get_comics
+    @offset = params.fetch(:offset, 0).to_i
+    @offset = 0 if @offset <= 0
+    @comics = Comic.get_comics(@offset)
     @comics
   end
 end
--- app/models/comic.rb
+++ app/models/comic.rb
@@ -2,14 +2,23 @@ class Comic < ApplicationRecord
   require 'digest/md5'
   require 'rest-client'
 
-  def self.get_comics
-    parsed_data = JSON.parse(call_api)
-    parsed_data['data']['results'].map { |results| {"id" => results["id"], "title" => results["title"], "image" => results["thumbnail"]["path"]} }
+  def self.get_comics offset
+    parsed_data = JSON.parse(call_api_fetch_comics(offset))
+    parsed_data['data']['results'].map { |results| {"id" => results["id"], "title" => results["title"], "image" => image(results)} }
   end
 
   private
 
-  def self.call_api
-    RestClient.get("#{Rails.application.secrets.url}#{Rails.application.credentials[:MARVEL_API][:PUBLIC_KEY]}&hash=#{Digest::MD5.hexdigest(Rails.application.credentials[:MARVEL_API][:HASH])}&ts=1234&orderBy=-onsaleDate")
+  def self.call_api_fetch_comics offset
+    RestClient.get("#{Rails.application.secrets.url}comics?apikey=#{Rails.application.credentials[:MARVEL_API][:PUBLIC_KEY]}&hash=#{Digest::MD5.hexdigest(Rails.application.credentials[:MARVEL_API][:HASH])}&ts=1234&orderBy=-onsaleDate&offset=#{offset}")
+  end
+
+  def self.call_api_fetch_character name
+    RestClient.get("#{Rails.application.secrets.url}characters?apikey=#{Rails.application.credentials[:MARVEL_API][:PUBLIC_KEY]}&hash=#{Digest::MD5.hexdigest(Rails.application.credentials[:MARVEL_API][:HASH])}&ts=1234&name=#{name}")
+  end
+
+
+  def self.image results
+    "#{results["thumbnail"]["path"]}/portrait_uncanny.#{results["thumbnail"]["extension"]}"
   end
 end
